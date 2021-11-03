d7ee0eb0ac0f7f9115c286a7a2adbc4a32fdda37
sanekdi <progtime@mail.ru>
2021-11-03 08:04:29 +0500

    list persons in SinglePostComponennt

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,11 +1,31 @@
 <template>
-    <SinglePostComponent></SinglePostComponent>
+    <div>
+        <SinglePostComponent></SinglePostComponent>
+        <div>name: {{ name }}</div>
+        <div>job: {{ vasyaJob }}</div>
+        <button @click="sayHello">Say Hello</button>
+    </div>
 </template>
 
 <script>
 import SinglePostComponent from "./SinglePostComponent";
 export default {
     name: "PostComponent",
-    components: { SinglePostComponent }
+    components: { SinglePostComponent },
+    data() {
+        return {
+            name: "vasya"
+        };
+    },
+    methods: {
+        sayHello() {
+            console.log("hello");
+        }
+    },
+    computed: {
+        vasyaJob() {
+            return this.name + " Work in shop";
+        }
+    }
 };
 </script>
--- resources/js/components/SinglePostComponent.vue
+++ resources/js/components/SinglePostComponent.vue
@@ -1,11 +1,57 @@
 <template>
     <div>
-        Single Post
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Age</th>
+                    <th scope="col">Job</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr
+                    v-for="(person, i) in personAgeTwenty"
+                    :key="person.name + i"
+                >
+                    <th scope="row">{{ i + 1 }}</th>
+                    <td>{{ person.name }}</td>
+                    <td>{{ person.age }}</td>
+                    <td>{{ person.job }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
 export default {
-    name: "SinglePostComponent"
+    name: "SinglePostComponent",
+    data() {
+        return {
+            persons: [
+                {
+                    name: "Вася",
+                    age: 20,
+                    job: "Тренер"
+                },
+                {
+                    name: "Петя",
+                    age: 25,
+                    job: "Менед"
+                },
+                {
+                    name: "Миша",
+                    age: 22,
+                    job: "Студент"
+                }
+            ]
+        };
+    },
+    computed: {
+        personAgeTwenty() {
+            return this.persons.filter(person => person.age > 20);
+        }
+    }
 };
 </script>

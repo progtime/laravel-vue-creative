d5ba89611c4dd776cf1584263e894dce818216ed
sanekdi <progtime@mail.ru>
2021-11-06 15:36:57 +0500

    update person to toggle form

--- resources/js/components/Person/IndexPerson.vue
+++ resources/js/components/Person/IndexPerson.vue
@@ -2,23 +2,48 @@
     <div class="p-3">
         <h1>Список пользователей</h1>
         <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Name</th>
-      <th scope="col">Age</th>
-      <th scope="col">Job</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="person in persons" :key="person.id">
-      <th scope="row">{{person.id}}</th>
-      <td>{{person.name}}</td>
-      <td>{{person.age}}</td>
-      <td>{{person.job}}</td>
-    </tr>
-  </tbody>
-</table>
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Age</th>
+                    <th scope="col">Job</th>
+                    <th scope="col">Edit</th>
+                </tr>
+            </thead>
+            <tbody>
+                <template v-for="person in persons">
+                    <tr>
+                        <th scope="row">{{ person.id }}</th>
+                        <td>{{ person.name }}</td>
+                        <td>{{ person.age }}</td>
+                        <td>{{ person.job }}</td>
+                        <td>
+                            <a
+                                href="#"
+                                @click.prevent="changeEditPersonId(person.id)"
+                                class="btn btn-success"
+                                >Edit</a
+                            >
+                        </td>
+                    </tr>
+                    <tr :class="isEdit(person.id) ? '' : 'd-none'">
+                        <th scope="row">{{ person.id }}</th>
+                        <td><input type="text" class="form-control" /></td>
+                        <td><input type="number" class="form-control" /></td>
+                        <td><input type="text" class="form-control" /></td>
+                        <td>
+                            <a
+                                href="#"
+                                @click.prevent="changeEditPersonId(null)"
+                                class="btn btn-success"
+                                >Update</a
+                            >
+                        </td>
+                    </tr>
+                </template>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -27,19 +52,24 @@ export default {
     name: "IndexPerson",
     data() {
         return {
-           persons: []
+            persons: [],
+            editPersonId: null
         };
     },
     mounted() {
         this.getPeoples();
     },
     methods: {
-        getPeoples(){
-            axios
-            .get('/api/people')
-            .then(res => {
+        getPeoples() {
+            axios.get("/api/people").then(res => {
                 this.persons = res.data;
-            })
+            });
+        },
+        changeEditPersonId(id) {
+            this.editPersonId = id;
+        },
+        isEdit(id) {
+            return this.editPersonId === id;
         }
     }
 };
